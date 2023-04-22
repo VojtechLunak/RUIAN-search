d1b8e95fb3f2d6ab40acd5ce663329b8f31026a8
Vojtěch Luňák <vojtech.lunak1@gmail.com>
2023-04-22 12:26:12 +0200

    Fix to run solr in the foreground to not kill the container right after script execution

--- solr/ruian-solr.sh
+++ solr/ruian-solr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 solr-precreate ruian "/opt/solr/server/solr/configsets/ruianConfig" # maybe solr-create ?
-solr start -force
+#solr start -force
+solr-foreground -force
 
 /opt/ruian-solr-scripts/initial-update.sh
 
